898b50371f4a500d6b25c486047d9507e61c6a2d
maiha <maiha@wota.jp>
2020-04-01 17:01:59 +0900

    easy: Human module should be located under Easy

--- src/curl/easy/human.cr
+++ src/curl/easy/human.cr
@@ -1,13 +1,15 @@
-module Curl::Human
-  abstract def status : Status
-  abstract def info   : Info
+class Curl::Easy
+  module Human
+    abstract def status : Status
+    abstract def info   : Info
 
-  def human_code(default = nil) : String
-    case status
-    when .free? ; default || "---"
-    when .run?  ; "RUN"
-    when .done? ; (info.response_code == 0) ? "ERR" : info.response_code.to_s
-    else        ; "BUG"         # enum error
+    def human_code(default = nil) : String
+      case status
+      when .free? ; default || "---"
+      when .run?  ; "RUN"
+      when .done? ; (info.response_code == 0) ? "ERR" : info.response_code.to_s
+      else        ; "BUG"         # enum error
+      end
     end
   end
 end
